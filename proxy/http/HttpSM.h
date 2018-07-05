8c11a9f2fdd9c7c781a62dec18c00f8ed4558010
Pushkar Pradhan <ppradhan@oath.com>
2018-07-05 15:16:52 -0700

    WIP: Code won't compile

--- proxy/http/HttpSM.h
+++ proxy/http/HttpSM.h
@@ -209,6 +209,328 @@ public:
   ~PostDataBuffers();
 };
 
+class BasicSM : public Continuation
+{
+
+public:
+  BasicSM();
+  virtual void cleanup() = 0;
+  virtual void destroy() = 0;
+
+  HttpVCTableEntry *get_ua_entry(); // Added to get the ua_entry pointer  - YTS-TEAM
+
+  virtual void init() = 0;
+
+  virtual void attach_client_session(ProxyClientTransaction *client_vc_arg, IOBufferReader *buffer_reader) = 0;
+
+  // Called by httpSessionManager so that we can reset
+  //  the session timeouts and initiate a read while
+  //  holding the lock for the server session
+  virtual void attach_server_session(HttpServerSession *s) = 0;
+
+  // Used to read attributes of
+  // the current active server session
+  HttpServerSession *
+  get_server_session()
+  {
+    return server_session;
+  }
+
+  // Called by transact.  Updates are fire and forget
+  //  so there are no callbacks and are safe to do
+  //  directly from transact
+  virtual void do_hostdb_update_if_necessary() = 0;
+
+  // Called by transact to prevent reset problems
+  //  failed PUSH requests
+  void set_ua_half_close_flag();
+
+  // Called by either state_hostdb_lookup() or directly
+  //   by the HostDB in the case of inline completion
+  // Handles the setting of all state necessary before
+  //   calling transact to process the hostdb lookup
+  // A NULL 'r' argument indicates the hostdb lookup failed
+  void process_hostdb_info(HostDBInfo *r);
+  void process_srv_info(HostDBInfo *r);
+
+  // Called from InkAPI.cc which acquires the state machine lock
+  //  before calling
+  int state_api_callback(int event, void *data);
+  int state_api_callout(int event, void *data);
+
+  // Debugging routines to dump the SM history, hdrs
+  virtual void dump_state_on_assert() = 0;
+
+  // Functions for manipulating api hooks
+  void txn_hook_append(TSHttpHookID id, INKContInternal *cont);
+  void txn_hook_prepend(TSHttpHookID id, INKContInternal *cont);
+  APIHook *txn_hook_get(TSHttpHookID id);
+
+  virtual bool is_private();
+  virtual bool is_redirect_required();
+
+  /// Get the protocol stack for the inbound (client, user agent) connection.
+  /// @arg result [out] Array to store the results
+  /// @arg n [in] Size of the array @a result.
+  virtual int populate_client_protocol(std::string_view *result, int n) const;
+  virtual const char *client_protocol_contains(std::string_view tag_prefix) const;
+  virtual std::string_view find_proto_string(HTTPVersion version) const;
+
+protected:
+  HttpVCTable vc_table;
+
+  HttpVCTableEntry *ua_entry = nullptr;
+  void remove_ua_entry();
+
+public:
+  ProxyClientTransaction *ua_txn   = nullptr;
+  // AuthHttpAdapter authAdapter;
+
+  History<HISTORY_DEFAULT_SIZE> history;
+
+protected:
+  IOBufferReader *ua_buffer_reader     = nullptr;
+  IOBufferReader *ua_raw_buffer_reader = nullptr;
+
+  HttpVCTableEntry *server_entry    = nullptr;
+  HttpServerSession *server_session = nullptr;
+
+  /* Because we don't want to take a session from a shared pool if we know that it will be private,
+   * but we cannot set it to private until we have an attached server session.
+   * So we use this variable to indicate that
+   * we should create a new connection and then once we attach the session we'll mark it as private.
+   */
+  bool will_be_private_ss              = false;
+  int shared_session_retries           = 0;
+  IOBufferReader *server_buffer_reader = nullptr;
+  void remove_server_entry();
+
+  BasicSMHandler default_handler = nullptr;
+  Action *pending_action        = nullptr;
+  Continuation *schedule_cont   = nullptr;
+
+  virtual void start_sub_sm() = 0;
+
+  int main_handler(int event, void *data);
+  int tunnel_handler(int event, void *data);
+  int tunnel_handler_push(int event, void *data);
+  int tunnel_handler_post(int event, void *data);
+
+  // YTS Team, yamsat Plugin
+  int tunnel_handler_for_partial_post(int event, void *data);
+
+  void tunnel_handler_post_or_put(HttpTunnelProducer *p);
+
+  int state_watch_for_client_abort(int event, void *data);
+  int state_srv_lookup(int event, void *data);
+  int state_hostdb_lookup(int event, void *data);
+  int state_hostdb_reverse_lookup(int event, void *data);
+  int state_mark_os_down(int event, void *data);
+  int state_add_to_list(int event, void *data);
+  int state_remove_from_list(int event, void *data);
+
+  // Y! ebalsa: remap handlers
+  int state_remap_request(int event, void *data);
+  void do_remap_request(bool);
+
+  // Cache Handlers
+  int state_cache_open_read(int event, void *data);
+  int state_cache_open_write(int event, void *data);
+
+  // Http Server Handlers
+  int state_http_server_open(int event, void *data);
+  int state_raw_http_server_open(int event, void *data);
+  int state_send_server_request_header(int event, void *data);
+  int state_acquire_server_read(int event, void *data);
+  int state_read_server_response_header(int event, void *data);
+
+  // API
+  int state_request_wait_for_transform_read(int event, void *data);
+  int state_response_wait_for_transform_read(int event, void *data);
+  int state_common_wait_for_transform_read(HttpTransformInfo *t_info, BasicSMHandler tunnel_handler, int event, void *data);
+
+  // Tunnel event handlers
+  int tunnel_handler_server(int event, HttpTunnelProducer *p);
+  int tunnel_handler_ua(int event, HttpTunnelConsumer *c);
+  int tunnel_handler_ua_push(int event, HttpTunnelProducer *p);
+  int tunnel_handler_100_continue_ua(int event, HttpTunnelConsumer *c);
+  int tunnel_handler_cache_write(int event, HttpTunnelConsumer *c);
+  int tunnel_handler_cache_read(int event, HttpTunnelProducer *p);
+  int tunnel_handler_post_ua(int event, HttpTunnelProducer *c);
+  int tunnel_handler_post_server(int event, HttpTunnelConsumer *c);
+  int tunnel_handler_ssl_producer(int event, HttpTunnelProducer *p);
+  int tunnel_handler_ssl_consumer(int event, HttpTunnelConsumer *p);
+  int tunnel_handler_transform_write(int event, HttpTunnelConsumer *c);
+  int tunnel_handler_transform_read(int event, HttpTunnelProducer *p);
+  int tunnel_handler_plugin_agent(int event, HttpTunnelConsumer *c);
+
+  void do_hostdb_lookup();
+  void do_hostdb_reverse_lookup();
+  void do_cache_lookup_and_read();
+  void do_http_server_open(bool raw = false);
+  void send_origin_throttled_response();
+  void do_setup_post_tunnel(HttpVC_t to_vc_type);
+  void do_cache_prepare_write();
+  void do_cache_prepare_write_transform();
+  void do_cache_prepare_update();
+  void do_cache_prepare_action(HttpCacheSM *c_sm, CacheHTTPInfo *object_read_info, bool retry, bool allow_multiple = false);
+  void do_cache_delete_all_alts(Continuation *cont);
+  void do_auth_callout();
+  void do_api_callout();
+  void do_api_callout_internal();
+  void do_redirect();
+  void redirect_request(const char *redirect_url, const int redirect_len);
+  void do_drain_request_body();
+
+  void wait_for_full_body();
+
+  virtual void handle_api_return();
+  void handle_server_setup_error(int event, void *data);
+  void handle_http_server_open();
+  void handle_post_failure();
+  void mark_host_failure(HostDBInfo *info, time_t time_down);
+  void mark_server_down_on_client_abort();
+  void release_server_session(bool serve_from_cache = false);
+  void set_ua_abort(HttpTransact::AbortState_t ua_abort, int event);
+  int write_header_into_buffer(HTTPHdr *h, MIOBuffer *b);
+  int write_response_header_into_buffer(HTTPHdr *h, MIOBuffer *b);
+  void setup_blind_tunnel_port();
+  void setup_client_header_nca();
+  void setup_client_read_request_header();
+  void setup_push_read_response_header();
+  void setup_server_read_response_header();
+  void setup_cache_lookup_complete_api();
+  void setup_server_send_request();
+  void setup_server_send_request_api();
+  HttpTunnelProducer *setup_server_transfer();
+  void setup_server_transfer_to_cache_only();
+  HttpTunnelProducer *setup_cache_read_transfer();
+  void setup_internal_transfer(BasicSMHandler handler);
+  void setup_error_transfer();
+  void setup_100_continue_transfer();
+  HttpTunnelProducer *setup_push_transfer_to_cache();
+  void setup_transform_to_server_transfer();
+  void setup_cache_write_transfer(HttpCacheSM *c_sm, VConnection *source_vc, HTTPInfo *store_info, int64_t skip_bytes,
+                                  const char *name);
+  void issue_cache_update();
+  void perform_cache_write_action();
+  void perform_transform_cache_write_action();
+  void perform_nca_cache_action();
+  void setup_blind_tunnel(bool send_response_hdr, IOBufferReader *initial = nullptr);
+  HttpTunnelProducer *setup_server_transfer_to_transform();
+  HttpTunnelProducer *setup_transfer_from_transform();
+  HttpTunnelProducer *setup_cache_transfer_to_transform();
+  HttpTunnelProducer *setup_transfer_from_transform_to_cache_only();
+  void setup_plugin_agents(HttpTunnelProducer *p);
+
+  HttpTransact::StateMachineAction_t last_action     = HttpTransact::SM_ACTION_UNDEFINED;
+  int (BasicSM::*m_last_state)(int event, void *data) = nullptr;
+  virtual void set_next_state();
+  void call_transact_and_set_next_state(TransactEntryFunc_t f);
+
+  bool is_http_server_eos_truncation(HttpTunnelProducer *);
+  bool is_bg_fill_necessary(HttpTunnelConsumer *c);
+  int find_server_buffer_size();
+  int find_http_resp_buffer_size(int64_t cl);
+  int64_t server_transfer_init(MIOBuffer *buf, int hdr_size);
+
+public:
+  // TODO:  Now that bodies can be empty, should the body counters be set to -1 ? TS-2213
+  // Stats & Logging Info
+  int client_request_hdr_bytes       = 0;
+  int64_t client_request_body_bytes  = 0;
+  int server_request_hdr_bytes       = 0;
+  int64_t server_request_body_bytes  = 0;
+  int server_response_hdr_bytes      = 0;
+  int64_t server_response_body_bytes = 0;
+  int client_response_hdr_bytes      = 0;
+  int64_t client_response_body_bytes = 0;
+  int cache_response_hdr_bytes       = 0;
+  int64_t cache_response_body_bytes  = 0;
+  int pushed_response_hdr_bytes      = 0;
+  int64_t pushed_response_body_bytes = 0;
+  bool client_tcp_reused             = false;
+  // Info about client's SSL connection.
+  bool client_ssl_reused          = false;
+  bool client_connection_is_ssl   = false;
+  const char *client_protocol     = "-";
+  const char *client_sec_protocol = "-";
+  const char *client_cipher_suite = "-";
+  int server_transact_count       = 0;
+  bool server_connection_is_ssl   = false;
+  bool is_waiting_for_full_body   = false;
+  bool is_using_post_buffer       = false;
+
+  TransactionMilestones milestones;
+  ink_hrtime api_timer = 0;
+  // The next two enable plugins to tag the state machine for
+  // the purposes of logging so the instances can be correlated
+  // with the source plugin.
+  const char *plugin_tag = nullptr;
+  int64_t plugin_id      = 0;
+
+  // hooks_set records whether there are any hooks relevant
+  //  to this transaction.  Used to avoid costly calls
+  //  do_api_callout_internal()
+  bool hooks_set = false;
+
+protected:
+  TSHttpHookID cur_hook_id = TS_HTTP_LAST_HOOK;
+  APIHook *cur_hook        = nullptr;
+
+  //
+  // Continuation time keeper
+  int64_t prev_hook_start_time = 0;
+
+  int cur_hooks                = 0;
+  HttpApiState_t callout_state = HTTP_API_NO_CALLOUT;
+
+  // api_hooks must not be changed directly
+  //  Use txn_hook_{ap,pre}pend so hooks_set is
+  //  updated
+  HttpAPIHooks api_hooks;
+
+  // The terminate flag is set by handlers and checked by the
+  //   main handler who will terminate the state machine
+  //   when the flag is set
+  bool terminate_sm         = false;
+  bool kill_this_async_done = false;
+  bool parse_range_done     = false;
+  virtual int kill_this_async_hook(int event, void *data);
+  void kill_this();
+  void update_stats();
+  void transform_cleanup(TSHttpHookID hook, HttpTransformInfo *info);
+  bool is_transparent_passthrough_allowed();
+  void plugin_agents_cleanup();
+
+public:
+  LINK(BasicSM, debug_link);
+
+public:
+  bool set_server_session_private(bool private_session);
+  bool
+  is_dying() const
+  {
+    return terminate_sm;
+  }
+
+  int
+  client_connection_id() const
+  {
+    return _client_connection_id;
+  }
+
+  int
+  client_transaction_id() const
+  {
+    return _client_transaction_id;
+  }
+
+private:
+  PostDataBuffers _postbuf;
+  int _client_connection_id = -1, _client_transaction_id = -1;
+};
+
 class HttpSM : public Continuation
 {
   friend class HttpPagesHandler;
